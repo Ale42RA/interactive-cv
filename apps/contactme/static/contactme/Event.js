51f4706d7887771a5ee825c16757f1c8423fbd0d
Ale42RA <alejandra.riv.ada@gmail.com>
2020-06-23 16:40:46 +0200

    My Interactive CV, sixth commit

--- apps/contactme/static/contactme/Event.js
+++ apps/contactme/static/contactme/Event.js
@@ -1,5 +1,7 @@
 function myFunction() {
-  alert("Thank you for the email!");
+  alert("Your mail app will open now with your email already written. \n
+          You just have to click send. \n
+          Thank you for the email!");
 
 
 }
